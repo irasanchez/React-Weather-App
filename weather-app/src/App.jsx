f3ec69433ab72d9dcc325b2e17592a828448141e
iradsanchez <ira.d.sanchez@gmail.com>
2019-03-14 21:42:36 -0700

    Add CRA, Sass, store, and main container

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./sass/App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="main-container">Main Container</div>
+      </div>
+    );
+  }
+}
+
+export default App;
